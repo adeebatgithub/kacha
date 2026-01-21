baf5f0930cc13f52e680dd3a055111c5644eff46
adeebatgithub <adeebdanish063@gmail.com>
2026-01-21 09:45:47 +0530

    patch 1.0.0

--- artfest/settings.py
+++ artfest/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,12 +75,26 @@ ASGI_APPLICATION = "artfest.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+        "OPTIONS": {
+            "charset": "utf8mb4",
+        },
     }
 }
+
+DATABASES["default"]["OPTIONS"]["init_command"] = (
+    "SET sql_mode='STRICT_TRANS_TABLES'"
+)
+
 
 CHANNEL_LAYERS = {
     "default": {

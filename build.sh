34e29e5ffc2380d85dfe47159345e529d0547a21
adeebatgithub <adeebdanish063@gmail.com>
2026-01-21 10:42:12 +0530

    patch 1.0.0

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ username = "root"
 password = "root"
 
 if not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password)
+    User.objects.create_superuser(username=username, password=password)
     print("Superuser created.")
 else:
     print("Superuser already exists. Skipping.")
